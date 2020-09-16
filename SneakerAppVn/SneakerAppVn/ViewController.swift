15aa8f8838e771ff617c4acfc71d30070a3be5e6
Minh Nhat Phung <minhnhat.ph@samsung.com>
2020-09-16 09:27:53 +0700

    Set Up

---
+++ SneakerAppVn/SneakerAppVn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SneakerAppVn
+//
+//  Created by Phùng Minh Nhật on 9/16/20.
+//  Copyright © 2020 Phùng Minh Nhật. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
